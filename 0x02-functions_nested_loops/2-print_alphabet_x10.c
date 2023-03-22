bb46c64a881ccf36a3eabdb96c22035890d189c8
chDouae <chaterdouae@gmail.com>
2023-03-22 15:52:25 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,21 @@
 #include "main.h"
+
+/**
+  *vi 2-print_alphabet_x10.c - Make alphabet x10 times
+  *Return: void
+  */
 void print_alphabet_x10(void)
 {
-       char c;
-       int i = 0;
- 
-         while (i <= 9)
-	 {
-         for (c = 'a'; c <= 'z'; c++)
-	 {
-         _putchar(c);
-	 }
-	 _putchar('\n');
-         i++;
-	 }
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
 }
