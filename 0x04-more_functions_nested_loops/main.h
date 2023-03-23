bd9f3afebcb50d7514312fe9c827423e2e0809bc
chDouae <chaterdouae@gmail.com>
2023-03-23 14:27:17 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+*File - main.h
+*Author: Genius villagers
+*Desc: Headers contain protype for al functions
+* used in 0x04-more_functions_nested_loops
+*/
+int _putchar(char c);
+int _isdigit(int c);
+int null(int a,int b);
